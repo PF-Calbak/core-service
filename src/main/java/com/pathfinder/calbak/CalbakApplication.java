abf5646d560fbd91a9744d2dab27a5e49dcdeaad
ji-circle <sp202401jw@gmail.com>
2026-06-06 00:48:48 +0900

    init: 로컬 MVP용 초기세팅

---
+++ src/main/java/com/pathfinder/calbak/CalbakApplication.java
@@ -0,0 +1,13 @@
+package com.pathfinder.calbak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalbakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalbakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pathfinder/calbak/CalbakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pathfinder.calbak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalbakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
